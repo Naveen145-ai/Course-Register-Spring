7b62dc2a4b29e8daecfd679d2f3d346583463e88
Naveen145-ai <naveensrinivas145@gmail.com>
2025-08-14 10:19:33 +0530

    first

---
+++ src/main/java/com/example/Course_Register/CourseRegisterApplication.java
@@ -0,0 +1,14 @@
+package com.example.Course_Register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseRegisterApplication.class, args);
+	}
+
+}
+
